dfaf1d2307a1c14fde8f4feee6e54e977b27be23
Julia Mason <juliagmason@gmail.com>
2020-06-22 14:26:06 -0400

    Fixed error--replacing zeros in n_tot as well as kg_tot. Jitter may not be a good idea--masks zeros for rare species.

--- R/Plot_spp_exploration.R
+++ R/Plot_spp_exploration.R
@@ -5,9 +5,10 @@
 # Data exploration, plot time series for each species
 
 library (tidyverse)
+library(lubridate)
 library (ggmap)
 
-spr_ds <- read_csv ("../Data/springsurvey.csv",
+spr_ds <- read_csv ("Data/springsurvey.csv",
                     col_types = cols(
                       sample_id = col_factor(),
                       species = col_factor(),
@@ -16,7 +17,7 @@ spr_ds <- read_csv ("../Data/springsurvey.csv",
                     )) %>%
   mutate (date = make_date (year, month))
 
-aut_ds <- read_csv ("../Data/autumnsurvey.csv",
+aut_ds <- read_csv ("Data/autumnsurvey.csv",
                     col_types = cols(
                       sample_id = col_factor(),
                       species = col_factor(),
@@ -49,7 +50,8 @@ spp_tot_ds <- comb_ds %>%
 
 # list of species that have data
 sampled_spp <- sort(unique (spp_tot_ds$species))
-# plot dotplot, histogram, bar graph for each species
+
+# plot dotplot, histogram, bar graph for each species----
 cod_ds <- spp_tot_ds %>%
   filter (species == 1)
 
@@ -140,9 +142,10 @@ plot_spp_ts_fun <- function (spp) {
   spp_subset <- spp_tot_ds %>%
     ungroup() %>%
     filter (species == spp) %>%
-    select (n_tot, kg_tot, year, season) %>%
+    dplyr::select (n_tot, kg_tot, year, season) %>%
     full_join (all_yrs, by = c("year", "season")) %>%
-    mutate (kg_tot = replace(kg_tot, is.na(kg_tot) & (season == "spring" |  (season == "autumn" & year %in% 1995:2019)), 0))
+    mutate (kg_tot = replace(kg_tot, is.na(kg_tot) & (season == "spring" |  (season == "autumn" & year %in% 1995:2019)), 0),
+            n_tot = replace(n_tot, is.na(n_tot) & (season == "spring" |  (season == "autumn" & year %in% 1995:2019)), 0))
   
   spp_mns <- spp_subset %>%
     group_by (year, season) %>%
@@ -151,7 +154,7 @@ plot_spp_ts_fun <- function (spp) {
   
   
   p1 <- ggplot (spp_subset, aes (x = year, y= kg_tot, col = season)) +
-    geom_jitter (alpha = 0.5) +
+    geom_point (alpha = 0.5) +
     geom_smooth (method = "lm") +
     ggtitle ("Biomass jitter") 
   
@@ -197,8 +200,22 @@ dev.off()
 yr_breaks <- seq(1985, 2020, 5) # this cuts out 1985, going to say that's ok for now
 
 #ggmap basemap
-isl_basemap <- get_map (location = c(-30, 62, -8, 70), source = "stamen", maptype = "toner")
+#isl_basemap <- get_stamenmap (bbox = c(-30, 62, -8, 70), maptype = "toner-lite")
 
+# just do outline
+library (maps)
+library (mapdata)
+
+#https://www.r-spatial.org/r/2018/10/25/ggplot2-sf.html
+library (sf)
+library (rnaturalearth)
+library (rnaturalearthdata)
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+
+plot_spp_distr_fun <- function (spp) {
+  
 spp_distr_blocks <- spp_tot_ds %>%
   ungroup() %>%
   filter (species == spp) %>%
@@ -207,8 +224,53 @@ spp_distr_blocks <- spp_tot_ds %>%
           year_bn = cut(year, breaks = yr_breaks)) %>%
   filter (!is.na(year_bn)) #just cut out 1985 for now
 
-ggmap(isl_basemap) +
+plt <- ggplot(data = world) +
+  geom_sf() +
+  coord_sf (xlim = c(-30, -8), ylim = c (62,70), expand = FALSE ) +
   geom_point (aes (x = lon, y = lat, col = season, size = kg_tot),alpha = 0.7, data = spp_distr_blocks) +
   facet_wrap (~year_bn) +
-  ggtitle ("capelin distribution")
+  ggtitle (paste (spp_list$Scientific_name[which(spp_list$Spp_ID == spp)], ", ",
+                  spp_list$Common_name[which(spp_list$Spp_ID == spp)], sep = "")
+           ) +
+  theme_bw() +
+  theme (axis.text = element_blank())
+
+print(plt)
+
+#print (spp_list$Common_name[which(spp_list$Spp_ID == spp)])
   
+}
+
+pdf (file = "Figures/Spp_distribution.pdf")
+
+lapply (sampled_spp, plot_spp_distr_fun)
+
+dev.off()
+
+# just plot number of samples each year for eacn species
+
+# plot check n samples over time
+n_samples <- comb_samples %>%
+  group_by (season, year) %>%
+  tally() %>%
+  ggplot (aes (x = year, y = n, col = season)) +
+  geom_point() +
+  geom_line()
+# not sure this is helpful
+
+spp_n_samples <- spp_tot_ds %>%
+  group_by (species, season, year) %>%
+  tally()
+
+plot_spp_samples_ts <- function(spp){
+  spp_samples <- filter (spp_n_samples, species == spp)
+  
+  print(
+    ggplot (spp_samples, aes (x = year, y = n, col = season)) +
+      geom_point() +
+      geom_line() +
+      geom_smooth (method = "lm") +
+      ggtitle(paste("Number of samples present, ", spp_list$Scientific_name[which(spp_list$Spp_ID == spp)], ", ",
+                                                        spp_list$Common_name[which(spp_list$Spp_ID == spp)], sep = "")) 
+  )
+}
