ab7c8feccadb9157a640370910201a29c45fa637
Julia Mason <juliagmason@gmail.com>
2020-06-22 14:25:02 -0400

    script and csvs for combined autumn and spring data. moved autumn and spring into a "raw data" folder

---
+++ R/Write_combined_MFRI_survey_csvs.R
@@ -0,0 +1,41 @@
+## write combined datasets
+# 6/22/2020
+# JGM
+
+# write and save combined spring and autumn datasets
+spr_ds <- read_csv ("../Data/Raw_data/springsurvey.csv",
+                    col_types = cols(
+                      sample_id = col_factor(),
+                      species = col_factor(),
+                      stat_sq = col_factor() # staves off trailing ch parsing error
+                      
+                    )) %>%
+  mutate (date = make_date (year, month))
+
+aut_ds <- read_csv ("../Data/Raw_data/autumnsurvey.csv",
+                    col_types = cols(
+                      sample_id = col_factor(),
+                      species = col_factor(),
+                      stat_sq = col_factor() # staves off trailing ch parsing error
+                      
+                    ))%>%
+  mutate (date = make_date (year, month))
+
+# combined dataset
+comb_ds <- rbind (spr_ds, aut_ds) 
+comb_ds$season = c(rep ("spring", nrow (spr_ds)), rep("autumn", nrow (aut_ds)))
+
+write.csv (comb_ds, file = "Data/MFRI_comb_survey.csv", row.names = FALSE)
+
+# also create datasets with just station points, for temp and gear info
+spr_samples <- spr_ds %>%
+  distinct_at ("sample_id", .keep_all = TRUE)
+
+aut_samples <- aut_ds %>%
+  distinct_at ("sample_id", .keep_all = TRUE)
+
+#combine
+comb_samples <- rbind (spr_samples, aut_samples)
+comb_samples$season = c(rep ("spring", nrow (spr_samples)), rep("autumn", nrow (aut_samples)))
+
+write.csv (comb_samples, file = "Data/MFRI_comb_survey_samples.csv", row.names = FALSE)
