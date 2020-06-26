d9592dbb9de3fc9582109eef4f439efe9d190147
Julia Mason <juliagmason@gmail.com>
2020-06-26 10:42:26 -0400

    Fixed issue with find_bottom function (closed netcdf at end of function so wasn't returning the bottom temp/sal). Wrote monthly bottom temp raster.

--- R/Find_GINS_bottom_temp.R
+++ R/Find_GINS_bottom_temp.R
@@ -9,15 +9,17 @@ library (raster)
 ### Bottom temperature for seasonal GINS ----
 #=======================================
 # Previously worked with this in MATLAB
-# messy folder with both .nc and converted ".nc.mat" files from matlab attempts, so extract just ".nc" filenames
+# messy folder with both .nc and converted ".nc.mat" files from matlab attempts, so extract just ".nc" filenames by taking all ".nc" files and subtracting the ".nc.mat" files
 gins_files_all <- list.files (path = "../Documents/MATLAB/GINS", pattern = ".nc", full.names = T)
 gins_files_mat <- list.files (path = "../Documents/MATLAB/GINS", pattern = ".mat", full.names = T)
 gins_files <- setdiff (gins_files_all, gins_files_mat)
 
+# open one to test
 gins_tmp <- nc_open(gins_files[1])
 print (gins_tmp)
 gins_tan <- ncvar_get (gins_tmp, "t_an") # 241 x 141 x 102
 
+# Save gins_depths for future reference
 gins_depth <- ncvar_get (gins_tmp, "depth") # 50m increments from 0-2000m, then 100m increments 2100-5500
 save (gins_depth, file = "Data/gins_depths.RData")
 
@@ -33,14 +35,23 @@ gins_files_season <- list ("Winter" = gins_w_files,
                            "Fall" = gins_f_files)
 
 
-## create function that goes through my list of lists. each input will be a list of 3 filenames for the three decades I need. 
+# create function that goes through my list of lists. each input will be a list of 3 filenames for the three decades I need. 
 
 find_bottom <- function (filenames){
   gins <- lapply (filenames, nc_open)
   gins_t <- lapply (gins, function (x) ncvar_get(x, "t_an")) # each is 241 x 141 x 102
-  gins_arr <- array (unlist (gins_t), c (241, 141, 102, 3)) # put the three decades side by side in an array so I can take a mean
-  gins_mn <- apply (gins_arr, 1:3, mean, na.rm = TRUE) # take mean along 4th dimension (decade)
-  gins_bottom <- apply (gins_mn, c(1,2), function(x) x[max(which(!is.na(x)))]) # find deepest non-NA value along 3rd (depth) dimension
+  
+  # close nc files
+  lapply (gins, nc_close)
+  
+  # put the three decades side by side in an array so I can take a mean
+  gins_arr <- array (unlist (gins_t), c (241, 141, 102, 3)) 
+  
+  # take mean along 4th dimension (decade)
+  gins_mn <- apply (gins_arr, 1:3, mean, na.rm = TRUE) 
+  
+  # find deepest non-NA value along 3rd (depth) dimension
+  gins_bottom <- apply (gins_mn, c(1,2), function(x) x[max(which(!is.na(x)))]) 
 }
 
 gins_bt <- lapply (gins_files_season, find_bottom) # labeled list with bottom temperature for each
@@ -51,6 +62,7 @@ lon <- ncvar_get (gins_tmp, "lon")
 
 image (lon, lat, gins_bt$Summer)
 
+# save as RData file
 save (gins_bt, file = "Data/GINS_bottom_temp.RData")
 
 ##############################################
@@ -58,27 +70,28 @@ save (gins_bt, file = "Data/GINS_bottom_temp.RData")
 #############################################
 # 6/25/2020
 
-# Downloaded via python xarray. Three netcdf files of each: 85-94, 95-04, 05-12. 
+# Downloaded via python xarray. Three netcdf files, each for a decade: 85-94, 95-04, 05-12. 
 
-# Salinity data---
+# find bottom function that takes different variables----
+find_bottom_gins <- function (filename, var){
+  
+  gins <- nc_open(filename)
+  gins_t <- ncvar_get(gins, var) # 241 x 141 x 57 x 12
+  nc_close (gins)
+  
+  # find deepest non-NA value along 3rd (depth) dimension
+  gins_bottom <- apply (gins_t, c(1,2,4), function(x) x[max(which(!is.na(x)))]) 
+  
+}
+
+# Salinity data----
 
 # salinity files by decade, downloaded in python notebook
 gins_sal_files <- list.files (path = "../Documents/Python Scripts/", pattern = "GINS_sal_", full.names = T)
 
+gins_bottom_sal <- lapply (gins_sal_files, find_bottom_gins) # list with 3 elements
 
-# edit function to take different variables
-find_bottom_gins <- function (filename, var){
-  gins <- nc_open(filename)
-  gins_t <- ncvar_get(gins, var) # 241 x 141 x 57 x 12
-  gins_bottom <- apply (gins_t, c(1,2,4), function(x) x[max(which(!is.na(x)))]) # find deepest non-NA value along 3rd (depth) dimension
-  nc_close (gins)
-}
-
-
-gins_bottom_sal <- lapply (gins_sal_files, find_bottom_sal, var = "s_an")
-# list with 3 elements
-
-# make into a raster brick ----
+# make into a raster brick
 
 # turn list into array
 gins_bottom_sal_array <- array (unlist (gins_bottom_sal), 
@@ -116,8 +129,6 @@ gins_sal <- writeRaster(gins_sal_r, filename = "Data/GINS_bottom_sal.grd")
 # Temperature data ----
 gins_temp_files <- list.files (path = "../Documents/Python Scripts/", pattern = "GINS_temp_", full.names = T)
 
-test <- find_bottom_gins (gins_temp_files[2], var = "t_an")
-
 gins_bottom_temp <- lapply (gins_temp_files, find_bottom_gins, var = "t_an")
 
 # make into array
@@ -125,3 +136,17 @@ gins_bt_arr <- array (unlist (gins_bottom_temp),
                       c(241, 141, 36))
 
 image (lon, lat, gins_bt_arr[,,1])
+
+# transpose and rasterize
+gins_bt_rot <- aperm(gins_bt_arr, c(2, 1,3))
+gins_bt_rev <- apply (gins_bt_rot, c(2,3), rev)
+
+gins_bt_r <- brick(gins_bt_rev)
+projection(gins_bt_r) <- CRS(projstring)
+extent (gins_bt_r) <- c(xmn = min(latlons$lon), xmx = max (latlons$lon), ymn = min(latlons$lat), ymx = max(latlons$lat))
+
+
+plot (gins_bt_r, 1)
+
+# Write and save raster
+gins_bt <- writeRaster(gins_bt_r, filename = "Data/GINS_bottom_temp.grd")
