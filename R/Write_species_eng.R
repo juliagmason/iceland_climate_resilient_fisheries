f52990b7f2dff10f32415f50c28dde72bc9119d9
Julia Mason <juliagmason@gmail.com>
2020-07-10 17:24:56 -0400

    Added columns to species data table with number of years sampled in each season. This should allow me to quickly subset major species when fitting many models/running many plots.

--- R/Write_species_eng.R
+++ R/Write_species_eng.R
@@ -3,30 +3,35 @@
 #JGM
 
 
-# Notes from Pamela: 
+#### Notes from Pamela: ----
 # I see – those landings are from Hagstofa (Statistics Iceland) which are not always the same as Fiskistofa (Fisheries Directorate) and they lump all redfish together. You can try querying on the Fiskistofa database if you want, or just let me know what you want and we can pass landings data to you (ours should be on par with Fiskistofa, Hagstofa is less accurate).
-# 
-# I also checked with our redfish guy and he said the same as Magnús and that we don’t really track 11 with the surveys – the surveys don’t really cover its range. I also forgot to mention that the autumn survey is mainly for deeper water species, so we use it mainly to track Greenland halibut, redfish, blue ling, and greater silver smelt (and I’m probably forgetting some but you can check in the advice sheets on (https://www.hafogvatn.is/en/harvesting-advice), and most of the others are tracked using the spring survey. Many may have enough numbers to be tracked in both, but they sometimes have differing patterns. Many of the noncommercial species are never looked at.
-#                                                                                            The vast majority of grenadier landings are Coryphaenoides rupestrus, with a little Macrourus berglax. These are probably also lumped by Hagstofa.
-# 
-#                                                                                            All mackeral landings should be Scomber scombrus. T. trachurus is not caught around here as far as I know, but maybe incidentally in some pelagic surveys. 
-                                                                                                                                                                                                                                                                                                                                                    
 
+# I also checked with our redfish guy and he said the same as Magnús and that we don’t really track 11 with the surveys – the surveys don’t really cover its range. I also forgot to mention that the autumn survey is mainly for deeper water species, so we use it mainly to track Greenland halibut, redfish, blue ling, and greater silver smelt (and I’m probably forgetting some but you can check in the advice sheets on (https://www.hafogvatn.is/en/harvesting-advice), and most of the others are tracked using the spring survey. Many may have enough numbers to be tracked in both, but they sometimes have differing patterns. Many of the noncommercial species are never looked at.
+
+#The vast majority of grenadier landings are Coryphaenoides rupestrus, with a little Macrourus berglax. These are probably also lumped by Hagstofa.
+
+# All mackeral landings should be Scomber scombrus. T. trachurus is not caught around here as far as I know, but maybe incidentally in some pelagic surveys. 
+                                                                                                                                                                                                                                                                                                                                                    
+##### Clean and translate raw spp list
 # Species table has parentheses, icelandic names. Clean up so I can match species ID to common name and scientific name. 
 library (tidyverse)
 library (stringr)
 
 spp_table <- read_csv ("Data/Raw_data/species.csv")
 
+# The scientific names column ('visindaheiti') has notes on classification source and year. In some cases, there are sub-species names. I want to try to just take the first two words, or just one word if it's at the genus/family level. 
+
 #https://stackoverflow.com/questions/44689640/using-str-extract-all-to-extract-only-first-two-words-in-r
 # their command works for most scientific names, but not if there's only one word (Family etc.) need to do an if/else?
 
+# If there's a space in the visindaheiti column, take the first two words.
 spp_table$Scientific_name <- ifelse (
   grepl (" ", spp_table$visindaheiti), 
          word (spp_table$visindaheiti, 1, 2, sep = " "),
   spp_table$visindaheiti
   )
 
+# translate column headers and arrange in order of species ID
 spp_table_eng <- spp_table %>%
   dplyr::select (tegund, enskt_heiti, Scientific_name) %>%
   rename (Spp_ID = tegund,
@@ -35,15 +40,14 @@ spp_table_eng <- spp_table %>%
 
 # some have common name but not species name
 
-# add landings data landed names ----
-ldgs <- read_csv ("../Data/Landings_annual_all_sectors.csv")
+# add landings data landed names to be able to join landings data ----
+ldgs <- read_csv ("Data/Landings_annual_all_sectors.csv")
 
 prelim_names_match <- data.frame (Landed_name =  sort (unique (ldgs$species)),
                                   Common_name = sort (unique (ldgs$species))) %>% # second decoy column for joining
-  left_join (spp_list, by = "Common_name")
+  left_join (spp_table_eng, by = "Common_name")
 
 # this now has 42? two types of redfish 
-
 length (which (is.na (prelim_names_match$Spp_ID))) # 17 problem kids
 
 prelim_names_match[which(is.na (prelim_names_match$Spp_ID)),]
@@ -59,6 +63,7 @@ prelim_names_match[which(is.na (prelim_names_match$Spp_ID)),]
 #   arrange (desc (tot_cat))
 # only 10 and 62, and 10 is way more--247 total vs 13.3
 
+# make a reference table to match landing common names to spp_table common names
 spp_names_match <- prelim_names_match %>%
   mutate (Common_name =
             case_when (Landed_name == "Atlantic catfish" ~ "Atlantic wolffish",
@@ -92,7 +97,7 @@ spp_table_ldgs <- spp_table_eng %>%
            gsub(" ", "_", Scientific_name)
   )
     ) %>% # add underscore to match PA table. fill in with common name if no scientific name
-  filter (! Spp_ID %in% c(11, 953, 613, 913))
+  filter (! Spp_ID %in% c(11, 953, 613, 913)) # just take out repeated common name species, incl. s. mentella subspecies
 
 # fix repeated spp
 # have identified 3 so far that are commonly caught: S. mentella (11, 61), monkfish (953, 14), seminudus (69, 613), a. minor (13, 913)
@@ -101,6 +106,34 @@ spp_table_ldgs <- spp_table_eng %>%
 
 #comb_ds %>% filter (species %in% c(69, 613)) %>% group_by (species) %>% summarize (tot = sum(kg_per_nautmile, na.rm = TRUE))
 
-# add columns with number of years sampled and whether landed
+# add columns with number of years sampled and whether landed----
 
-write.csv (spp_table_ldgs, file = "Data/species_eng.csv", row.names = FALSE)
+comb_ds <- read_csv ("Data/MFRI_comb_survey.csv",
+                     col_types = cols(
+                       sample_id = col_factor(),
+                       species = col_factor(),
+                       stat_sq = col_factor())
+)
+
+
+# count how many years each species has samples for each season
+spp_yrs_sampled <- comb_ds %>%
+  group_by (species, season) %>%
+  summarise (n_yrs = length (unique (year))) %>%
+  pivot_wider (
+    names_from = season,
+    values_from = n_yrs
+  ) %>%
+  rename (Spp_ID = species,
+          n_autumn = autumn,
+          n_spring = spring) %>%
+  replace_na (list(n_autumn = 0, n_spring = 0))
+
+# not sure if it would be better to make spp_ID a factor for both, but switching to numeric so they're compatible
+spp_yrs_sampled$Spp_ID <- as.numeric(spp_yrs_sampled$Spp_ID)
+
+# join to spp_table by species
+spp_table_nyrs <- spp_table_ldgs %>%
+  left_join (spp_yrs_sampled, by = "Spp_ID")
+
+write.csv (spp_table_nyrs, file = "Data/species_eng.csv", row.names = FALSE)
