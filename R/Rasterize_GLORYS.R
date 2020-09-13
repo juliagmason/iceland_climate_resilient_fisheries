809135cee6befc80940035e440163ebdf544cde9
Julia Mason <juliagmason@gmail.com>
2020-09-13 17:40:57 -0400

    Re-did with monthly samples (16th of each month) of sst and bt, did focal sd with 3x3 matrix and rasterized those too.

--- R/Rasterize_GLORYS.R
+++ R/Rasterize_GLORYS.R
@@ -10,33 +10,76 @@ load ("Data/prediction_raster_template.RData") # named pred_r_template
 
 projstring <- '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
 
-# historical monthly means, 2000-2018. have not resampled. 
-glorys_sst_hist <- nc_open ("Data/glorys_sst_month_mean.nc")
-glorys_bt_hist <- nc_open ("Data/glorys_bt_month_mean.nc")
-print (glorys_sst_hist)
+#glorys_files <- list.files (path = "Data", pattern = "glorys.*month.*.nc") 
+glorys_files <- list.files (path = "Data", pattern = "glorys.*16.*.nc") 
 
-lat <- ncvar_get (glorys_sst_hist, 'latitude')
-lon <- ncvar_get (glorys_sst_hist, 'longitude')
-latlons <- expand.grid (lon, lat); colnames (latlons) <- c('lon', 'lat')
+rasterize_glorys_fun <- function (file) {
+  
+  # split file name to get the elements I want. want to take off .nc and also split on the underscore, so will split twice.
+  # remove .nc-will use this as the raster file name
+  file_base <- strsplit (file, "[.]")[[1]][1]
+  file_split <- strsplit (file_base, "_")[[1]]
+  
+  # open netcdf and get variables
+  glorys_nc <- nc_open (file.path ("Data", file))
+  
+  lat <- ncvar_get (glorys_nc, 'latitude')
+  lon <- ncvar_get (glorys_nc, 'longitude')
+  latlons <- expand.grid (lon, lat); colnames (latlons) <- c('lon', 'lat')
+  
+  # get either sst, 'thetao', or bottom temp, 'bottomT'
+  ifelse (file_split[2] == "sst", 
+          temp <- ncvar_get (glorys_nc, "thetao"),
+          temp <- ncvar_get (glorys_nc, "bottomT"))
+  
+  # transpose, rotate, and convert to brick
+  t_rot <- aperm (temp, c (2, 1, 3))
+  t_rev <- apply (t_rot, c(2,3), rev)
+  
+  temp_br <- brick (t_rev)
+  
+  # set brick projection and extent
+  projection (temp_br) <- CRS(projstring)
+  
+  extent (temp_br) <- c (xmn = min (latlons$lon),
+                       xmx = max (latlons$lon),
+                       ymn = min (latlons$lat),
+                       ymnx = max (latlons$lat))
+  
+  temp_resamp <- raster::resample (temp_br, pred_r_template, method = "bilinear")
+  
+  # Write and save raster
+  writeRaster(temp_resamp, filename = paste0("Data/", file_base, ".grd"), overwrite = TRUE)
+  
+}
 
-sst <- ncvar_get (glorys_sst_hist, 'thetao') # 721 x 421 x 228
-bt <- ncvar_get (glorys_bt_hist, 'bottomT')
+sapply (glorys_files, rasterize_glorys_fun)
 
-image (lon, lat, bt[,,1])
+# Calculate and rasterize standard deviation----
 
-# transpose
-sst_rot <- aperm(bt, c(2, 1,3))
-sst_rev <- apply (sst_rot, c(2,3), rev)
+sst_gl <- brick ("Data/glorys_sst_16_sample.grd")
+bt_gl <- brick ("Data/glorys_sst_16_sample.grd")
+# function for applying focal statistics on a brick
+# https://stat.ethz.ch/pipermail/r-sig-geo/2016-May/024454.html
+multiFocal <- function(x, w=matrix(1, nr=3, nc=3), ...) {
+  
+  if(is.character(x)) {
+    x <- brick(x)
+  }
+  # The function to be applied to each individual layer
+  fun <- function(ind, x, w, ...){
+    focal(x[[ind]], w=w, ...)
+  }
+  
+  n <- seq(nlayers(x))
+  list <- lapply(X=n, FUN=fun, x=x, w=w, ...)
+  
+  out <- stack(list)
+  return(out)
+}
 
-sst_r <- brick(sst_rev)
-projection (sst_r) <- CRS(projstring)
-extent (sst_r) <- c (xmn = min (latlons$lon),
-                     xmx = max (latlons$lon),
-                     ymn = min (latlons$lat),
-                     ymnx = max (latlons$lat))
+sst_gl_dev <- multiFocal(sst_gl, fun = sd)
+system.time(bt_gl_dev <- multiFocal (bt_gl, fun = sd)) # 5mins
 
-sst_resamp <- raster::resample (sst_r, pred_r_template, method = "bilinear")
-
-# Write and save raster
-glorys_sst_hist <- writeRaster(sst_resamp, filename = "Data/GLORYS_sst_hist.grd")
-glorys_bt_hist <- writeRaster(sst_resamp, filename = "Data/GLORYS_bt_hist.grd")
+writeRaster (sst_gl_dev, filename = "Data/glorys_sst_dev.grd")
+writeRaster (bt_gl_dev, filename = "Data/glorys_bt_dev.grd")
