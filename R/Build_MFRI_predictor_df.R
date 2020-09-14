08cbdb2122a6dc923ea1a82c7570c2ce4a5cc7c5
Julia Mason <juliagmason@gmail.com>
2020-09-14 13:40:02 -0400

    Removed OISST and GINS bottom temp and salinity; added Bormicon habitat regions and GLORYS min, max and stdev

--- R/Build_MFRI_predictor_df.R
+++ R/Build_MFRI_predictor_df.R
@@ -3,16 +3,19 @@
 # JGM
 
 # Start to build dataframe for attaching environmental predictor variables to sample lat lon
+# pairing rasters to points: https://stackoverflow.com/questions/34294376/how-to-extract-data-from-a-rasterbrick
 
 library (tidyverse)
 library (raster)
 library (lubridate)
 
-# csv of MFRI tow information
+# csv of MFRI tow information ----
 mfri_samples <- read_csv ("Data/MFRI_comb_survey_samples.csv",
                           col_types = cols(
                             sample_id = col_factor(),
-                            stat_sq = col_factor()))
+                            stat_sq = col_factor())) %>%
+  # fix a few stat_sq typos with hyphens
+  mutate (stat_sq = gsub ("-", "", stat_sq))
 
 # make spatial points df for extracting environmental data
 mfri_pts <- mfri_samples %>%
@@ -21,128 +24,11 @@ mfri_pts <- mfri_samples %>%
 
 coordinates(mfri_pts) <- ~lon + lat
 
-# environmental data raster bricks
-# https://stackoverflow.com/questions/34294376/how-to-extract-data-from-a-rasterbrick
+# vector of unique dates to subset environmental brick layers
+mfri_dates <- unique (mfri_pts$date)
 
-# bottom temp, bottom salinity from GINS climatology
-gins_bt <- brick ("Data/GINS_bottom_temp.grd")
-gins_sal <- brick ("Data/GINS_bottom_sal.grd")
-
-# OISST 
-oisst <- brick ("Data/OISST_MFRI.grd")
-
-# Extract OISST ----
-
-mfri_dates <- unique (mfri_pts$date) # 106
-
-# set up vector of OISST dates
-oisst_dates <- seq (ymd("1985-01-01"), ymd("2020-06-01"), by = "months")
-
-# make empty df
-mfri_oisst_df <- data.frame(sample_id = factor(),
-                            oisst = numeric())
-
-for (x in mfri_dates) {
-  # subset mfri data
-  mfri <- mfri_pts[which (mfri_pts$date == x),]
-  
-  # subset oisst brick. y is the oisst_dates index that matches x date
-  y <- which (oisst_dates == x)
-  oisst_subset <- subset (oisst, y)
-  oisst_extract <- raster::extract (oisst_subset, mfri, method = "simple")
-  
-  x_df <- data.frame (sample_id = mfri$sample_id, oisst = oisst_extract)
-  
-  mfri_oisst_df <- rbind (mfri_oisst_df, x_df)
-}
-
-# wants sample_id to be ch for some reason...
-mfri_oisst_df$sample_id <- as.factor(mfri_oisst_df$sample_id)
-
-# Extract GINS bottom temp ----
-
-# This one is harder--values are averaged by decade and only go to 2012
-
-mfri_dates_gins <- unique (mfri_pts$date[which (mfri_pts$date < "2013-01-01")]) # 106 total dates; 73 before 2012
-
-gins_dates <- paste (
-  rep (c(1985, 1995, 2005), each = 12),
-  sprintf ("%02d", 1:12),
-  "01",
-  sep = "-")
-
-
-# make empty df
-mfri_bt_df <- data.frame(sample_id = factor(),
-                           gins_bt = numeric())
-
-# doesn't like x in mfri_dates_in for some reason?
-for (i in 1:length(mfri_dates_gins)) {
-  x <- mfri_dates_gins[i]
-  # subset mfri data
-  mfri <- mfri_pts[which (mfri_pts$date == x),]
-  
-  # match date by re-assigning year to appropriate decade
-  y <- case_when (
-    between (year(x), 1985, 1994) ~ paste0 ("1985", substr(x, 5, 10)),
-    between (year(x), 1995, 2004) ~ paste0 ("1995", substr(x, 5, 10)),
-    between (year(x), 2005, 2012) ~ paste0 ("2005", substr(x, 5, 10))
-  )
-  
-  #index date
-  y_in <- which (gins_dates == y)
-  
-  # subset oisst brick
-  gins_subset <- subset (gins_bt, y_in)
-  gins_extract <- raster::extract (gins_subset, mfri, method = "simple")
-  
-  x_df <- data.frame (sample_id = mfri$sample_id, gins_bt = gins_extract)
-  
-  mfri_bt_df <- rbind (mfri_bt_df, x_df)
-}
-
-mfri_bt_df$sample_id <- as.factor(mfri_bt_df$sample_id)
-
-# Extract GINS salinity ----
-# make empty df
-mfri_sal_df <- data.frame(sample_id = factor(),
-                         gins_sal = numeric())
-
-
-for (i in 1:length(mfri_dates_gins)) {
-  x <- mfri_dates_gins[i]
-  # subset mfri data
-  mfri <- mfri_pts[which (mfri_pts$date == x),]
-  
-  # match date by re-assigning year to appropriate decade
-  y <- case_when (
-    between (year(x), 1985, 1994) ~ paste0 ("1985", substr(x, 5, 10)),
-    between (year(x), 1995, 2004) ~ paste0 ("1995", substr(x, 5, 10)),
-    between (year(x), 2005, 2012) ~ paste0 ("2005", substr(x, 5, 10))
-  )
-  
-  #index date
-  y_in <- which (gins_dates == y)
-  
-  # subset oisst brick
-  gins_subset <- subset (gins_sal, y_in)
-  gins_extract <- raster::extract (gins_subset, mfri, method = "simple")
-  
-  x_df <- data.frame (sample_id = mfri$sample_id, gins_sal = gins_extract)
-  
-  mfri_sal_df <- rbind (mfri_sal_df, x_df)
-}
-
-mfri_sal_df$sample_id <- as.factor(mfri_sal_df$sample_id)
-
-# Attach env vars to data frame----
-
-mfri_env_df <- mfri_samples %>%
-  left_join (mfri_oisst_df, by = "sample_id") %>%
-  left_join (mfri_bt_df, by = "sample_id") %>%
-  left_join (mfri_sal_df, by = "sample_id")
-
-# 8/20/2020 update: adding habitat data from Bormicon regions ----
+# Habitat data from Bormicon regions ----
+# 8/20/2020 update: 
 # From Pamela: # I attached a table you can join with stat_sq column to get at this. We generally use 'Bormicon' regions, which were defined a while ago based on depth and cluster analyses (see link below), but not so much habitat data if I remember correctly (double check this!). That means they may be a bit y ~ x ~ y situation because I believe multi-species survey data were used to define them. The true 'habitat' studies taking place here (using side sonar and such) don't nearly cover enough area to be useful yet.
 # https://www.hafogvatn.is/static/research/files/fjolrit-119.pdf  see page 330 - division and subdivision numbers correspond with attached table
 
@@ -156,24 +42,100 @@ hab_table <- read.csv ("Data/Raw_data/bormicon_table.csv") %>%
   summarize (bormicon_region = first(subdivision)) %>%
   mutate (stat_sq = as.factor(stat_sq))
 
-# realized there are a few stat_sq typos where there are hyphens
-mfri_env_df <- read.csv ("Data/MFRI_predictor_df.csv") %>%
-  mutate (stat_sq = gsub ("-", "", stat_sq)) %>%
-  left_join (hab_table, by = "stat_sq")
 
-# only one missing, stat_Sq is 4214.   
-mfri_env_df[which (is.na(mfri_env_df$bormicon_region)),]  
+# GLORYS sst min, max, stdev ----
 
-# does lat/lon fall within 4124? no
-mfri_env_df %>%
-  filter (stat_sq == 4124) %>%
-  select (lat, lon) %>% View()
+# update 9/14/2020:
+# for max and min, I need to find the layer that corresponds to the point date, then take a subset representing the preceeding twelve layers. then I extract the cells corresponding to the points, and calculate the max or min. 
+
+sst_max <- brick ("Data/glorys_sst_month_max.grd")
+sst_min <- brick ("Data/glorys_sst_month_min.grd")
+bt_max <- brick ("Data/glorys_bt_month_max.grd")
+bt_min <- brick ("Data/glorys_bt_month_min.grd")
+
+
+# vector of corresponding MFRI dates, starting one year ahead
+mfri_dates_gl <- mfri_dates[which (between (mfri_dates, ymd("1994-01-01"), ymd("2018-12-01")))]
+
+# set up vector of glorys dates (actually took values from the 16th, but just use 01 here to match)
+glorys_dates <- seq (ymd("1993-01-01"), ymd("2018-12-01"), by = "months")
+
+# tmp--
+mfri_dates_inf <- 
+
+# make empty df
+mfri_glorys_df <- data.frame()
+
+for (x in mfri_dates_gl) {
+  # subset mfri data
+  mfri <- mfri_pts[which (mfri_pts$date == x),]
+  
+  # subset glorys brick with 12 preceding layers
+  y <- which (glorys_dates == x)
+  
+  # take a subset representing date and 12 months prior; extract values for each lat/lon point, and calculate max of those values
+  smax_extract <- apply (
+    z <- raster::extract (
+      subset (sst_max, (y-11):y),
+      mfri, method = "simple"), 
+    1, function (x) max (x, na.rm = TRUE))
+  
+  smin_extract <- apply (
+    raster::extract (
+      subset (sst_min, (y-11):y),
+      mfri, method = "simple"), 
+    1, function (x) min (x, na.rm = TRUE))
+  
+  bmax_extract <- apply (
+    raster::extract (
+      subset (bt_max, (y-11):y),
+      mfri, method = "simple"), 
+    1, function (x) max (x, na.rm = TRUE))
+  
+  bmin_extract <- apply (
+    raster::extract (
+      subset (bt_min, (y-11):y),
+      mfri, method = "simple"), 
+    1, function (x) min (x, na.rm = TRUE))
+  
+  # combine into data frame
+  x_df <- data.frame (sample_id = mfri$sample_id, 
+                      sst_max = smax_extract,
+                      sst_min = smin_extract,
+                      bt_max = bmax_extract,
+                      bt_min = bmin_extract)
+  
+  mfri_glorys_df <- rbind (mfri_glorys_df, x_df)
+}
+
+# there are 116 missing values that are too close to shore to be resolved I think
+
+# combine df ----
+
+mfri_env_df <- mfri_samples %>%
+  left_join (hab_table, by = "stat_sq") %>%
+  left_join (mfri_glorys_df, by = "sample_id")
+
 
 
 # write csv----
 write.csv (mfri_env_df, file = "Data/MFRI_predictor_df.csv", row.names = FALSE)
 
 # explore missing values ----
+
+# inf glorys
+gl_inf <- mfri_env_df[which (is.infinite(mfri_env_df$sst_max)),]
+
+ggplot(data = world) +
+  geom_sf() +
+  coord_sf (xlim = c(-30, -8), ylim = c (62,70), expand = FALSE ) +
+  geom_point (aes (x = lon, y = lat),alpha = 0.7, data = gl_inf) +
+  theme_bw() +
+  theme (axis.text = element_blank())
+
+
+mfri_inf <- mfri_pts[which (mfri_pts$sample_id %in% gl_inf$sample_id),]
+
 # plot check for missing gins values
 library (ncdf4)
 gins_temp_files <- list.files (path = "../Documents/Python Scripts/", pattern = "GINS_temp_", full.names = T)
