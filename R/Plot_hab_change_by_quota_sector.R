75eaa426be81c1e60987ecb98b5831fd88560bdf
Julia Mason <juliagmason@gmail.com>
2020-10-28 16:12:42 -0400

    Updated with Borm_14 model and select geom_text labels

--- R/Plot_hab_change_by_quota_sector.R
+++ R/Plot_hab_change_by_quota_sector.R
@@ -4,6 +4,7 @@
 # JGM
 
 library (tidyverse)
+library (ggrepel) # for cleaner geom_text labels
 
 # spp list to match landed and sci name
 spp_list <- read.csv("Data/species_eng.csv")
@@ -19,8 +20,7 @@ land_ds <- read.csv("Data/Landings_annual_all_sectors.csv")
 # this is community quota but I can't figure out where I got it....
 land_cq <- read.csv ("Data/comm_quota_landings_annual.csv")
 
-# this is all quota types, all species, with tonnes and value
-lands_qu <- read.csv("Data/Landings_annual_spp_quota.csv")
+
 
 # check most important species overall, by tonnes
 land_spp <- land_ds %>%
@@ -38,8 +38,92 @@ cq_spp <- land_cq %>%
              sd_cat = sd(tonnes, na.rm = TRUE)) %>%
   arrange (desc (mn_cat))
 
-# plot overall value vs habitat change----
 
+# divide out by sectors ----
+
+# this is all quota types, all species, with tonnes and value
+lands_qu <- read.csv("Data/Landings_annual_spp_quota.csv")
+
+# combine hook and line with small boats, per carothers 2017. also going to combine trawlers with large ITQ?
+spp_by_qu <- lands_qu %>%
+  filter (year >= 2010) %>%
+  mutate (Quota.class = case_when (
+    Quota.class == "Hook and line boats with catch quota" ~ "Small boats with catch quota", 
+    Quota.class == "Trawlers" ~ "Vessels with catch quota",
+    TRUE ~ Quota.class)
+  ) %>%
+  group_by (Quota.class, Species, metric) %>%
+  summarize (mn = mean (amount, na.rm = TRUE),
+             sd = sd(amount, na.rm = TRUE)) %>% 
+  pivot_wider (names_from = metric,
+               values_from = c(mn, sd)
+  ) %>%
+  filter (! is.na (mn_Tonnes)) %>%
+  rename (Landed_name = Species) %>% # match to spp list
+  left_join (spp_list, by = "Landed_name") %>%
+  rename (species = sci_name_underscore) # match to hab change
+#arrange (desc (mn_cat))
+
+# shortened quota names
+# https://www.datanovia.com/en/blog/how-to-change-ggplot-facet-labels/#:~:text=(dose%20~%20supp)-,Change%20the%20text%20of%20facet%20labels,labeller%20function%20label_both%20is%20used.
+quot_labs <- c("Coastal fisheries", "Small boat ITQ", "Large boat ITQ")
+names (quot_labs) <- c("Coastal fisheries", "Small boats with catch quota", "Vessels with catch quota")
+
+
+# plot sector volume/value vs habitat change----
+
+# borm 14
+borm_MASE <- read_csv ("Models/GAM_performance_Borm_14_alltemp.csv")
+
+load ("Data/perc_hab_change_Morely_Borm_14_alltemp.RData")
+
+
+# define outside so I can selectively label points
+hab_change_by_sector <- hab_change %>% 
+  group_by (species, scenario) %>%
+  summarise (mn_change = mean(perc_change),
+             sd_change = sd (perc_change)) %>%
+  right_join (spp_by_qu, by = "species") %>%
+  filter (scenario ==585, mn_Tonnes > 0)
+
+
+
+png (file = "Figures/Percent_hab_change_by_sector_importance_Borm_14.png", width = 16, height = 9, units = "in", res = 300)
+
+  ggplot (hab_change_by_sector,
+          aes (x = log(mn_Tonnes),
+               y = mn_change)
+          ) +
+  geom_point (aes(size = mn_X1000.ISK)) +
+  geom_text_repel (data = subset(hab_change_by_sector, 
+                                 mn_X1000.ISK > 3000000 | abs(mn_change) > 100 | (Quota.class == "Coastal fisheries" & mn_change > 0) | (Quota.class == "Coastal fisheries" & mn_change < -50) | (Quota.class == "Small boats with catch quota" & mn_X1000.ISK > 200000) | (Quota.class == "Small boats with catch quota" & mn_change > 70)),
+             aes (label = species)) +
+
+  facet_grid (scenario ~ Quota.class,
+              labeller = labeller (Quota.class = quot_labs)) +
+  geom_hline (yintercept = 0, lty = 2) +
+  geom_vline (xintercept = 0, lty = 2) +
+  xlim (-2, 15) + # for SLL
+  ylim (-100, 500) +
+  # xlim (-2, 15) +
+  # ylim (-100, 100) + # for depth temp
+  theme_bw () +
+  labs (x = "log (mean annual tonnes landed)",
+        y = "Percent habitat change",
+        size = "Mean annual value, \n1000 ISK") +
+  ggtitle ("Percent habitat change by fisheries sector") +
+  theme (
+    axis.text = element_text (size = 16),
+    axis.title = element_text (size = 18),
+    plot.title = element_text (size = 24),
+    strip.text = element_text (size = 18),
+    legend.text = element_text (size = 16),
+    legend.title = element_text (size = 18)
+  )
+dev.off()
+
+
+# SLL ----
 # fill based on certainty
 smoothLL_MASE <- read_csv ("Models/GAM_diagnostics_all_spp.csv") %>%
   rename (species = Species)  %>%
@@ -85,7 +169,7 @@ lands_qu %>%
 
 dev.off()
 
-# Temp/depth
+# Temp/depth ----
 TD_MASE <- read_csv ("Models/Temp_Depth_GAM_diagnostics.csv") %>%
   #rename (species = Species)  %>%
   mutate (Model_suitable = ifelse (
@@ -130,32 +214,6 @@ lands_qu %>%
 
 dev.off()
 
-# divide out by sectors ----
-
-# combine hook and line with small boats, per carothers 2017. also going to combine trawlers with large ITQ?
-spp_by_qu <- lands_qu %>%
-  filter (year >= 2010) %>%
-  mutate (Quota.class = case_when (
-    Quota.class == "Hook and line boats with catch quota" ~ "Small boats with catch quota", 
-    Quota.class == "Trawlers" ~ "Vessels with catch quota",
-    TRUE ~ Quota.class)
-    ) %>%
-  group_by (Quota.class, Species, metric) %>%
-  summarize (mn = mean (amount, na.rm = TRUE),
-                       sd = sd(amount, na.rm = TRUE)) %>% 
-  pivot_wider (names_from = metric,
-               values_from = c(mn, sd)
-               ) %>%
-  filter (! is.na (mn_Tonnes)) %>%
-  rename (Landed_name = Species) %>% # match to spp list
-  left_join (spp_list, by = "Landed_name") %>%
-  rename (species = sci_name_underscore) # match to hab change
-#arrange (desc (mn_cat))
-
-# shortened quota names
-# https://www.datanovia.com/en/blog/how-to-change-ggplot-facet-labels/#:~:text=(dose%20~%20supp)-,Change%20the%20text%20of%20facet%20labels,labeller%20function%20label_both%20is%20used.
-quot_labs <- c("Coastal fisheries", "Small boat ITQ", "Large boat ITQ")
-names (quot_labs) <- c("Coastal fisheries", "Small boats with catch quota", "Vessels with catch quota")
 
 # plot 
 
