2c2c523530281add9026fba965bccd5cca9b1259
Julia Mason <juliagmason@gmail.com>
2020-11-12 13:52:24 -0500

    Intermediate edits, still need to find and standardize CM2.1 and 2.6

--- R/Plot_Taylor_Diagrams.R
+++ R/Plot_Taylor_Diagrams.R
@@ -5,6 +5,8 @@ library (R.matlab) # for reading matlab files
 library (openair) # for taylor diagrams
 library (tidyverse) # for case_when
 
+# problem--all different dimensions. so that might be why CM 2.6 looks so off from everything else. 
+# CM 2.6: 1000 x 1018 x 4 CM_season_ESRL
 
 plot_TD_fun <- function (model_list, obs_var, figure_name) {
   # model is vector of 4 letter abbreviations, all caps
@@ -17,15 +19,17 @@ plot_TD_fun <- function (model_list, obs_var, figure_name) {
   if (obs_var == "OISST") {
     # load OISST. readMat brings in a list, the first element of which is the array I want
     # double check if ESRL_season_GFDL is the right one, but has the right dimensions
-    OISST_mat <- readMat ("../Documents/MATLAB/CMIP6/ESRL_season_GFDL.mat")
+    OISST_mat <- # read nc file in python scripts instead???
+      #readMat("../Documents/Python Scripts/oisst_month_climatology_1982_2011.nc.mat") not supporteded
+    #readMat ("../Documents/MATLAB/CMIP6/ESRL_season_GFDL.mat")
     OISST <- OISST_mat[[1]]
     
     # crop to square around Iceland
-    OISST_ISL_crop <- array(OISST[540:801, 601:661,])
+    OISST_ISL_crop <- array(OISST[397:801,521:720,])
     
   } else {
     # this one is wrong, find seasonal gins shaped to CMIP6
-    GINS_mat <- readMat ("../Documents/MATLAB/GINS/gins_seas.mat")
+    GINS_mat <- readMat ("../Documents/MATLAB/CMIP6/gins_bt_clim_reshape.mat") # 140 x 240 x 12 should be right... reshaped to OISST. 
     GINS <- GINS_mat[[1]]
     GINS_ISL_crop <- array (GINS[540:801, 601:661,])
   }
@@ -37,17 +41,18 @@ plot_TD_fun <- function (model_list, obs_var, figure_name) {
     
     # I have all the cmip6 files stored in one folder, but CM 2.6 and 2.1 in separate folders
     mat_file <- case_when (
-      model == "CM2_6" ~ "../Documents/MATLAB/CM2_6/CM26_season_ESRL.mat",
-      model == "CM2_1" ~ "../Documents/MATLAB/CM2_1/CM26_season_ESRL.mat",
-      TRUE ~ paste0 ("../Documents/MATLAB/CMIP6/", model, "_season_ESRL.mat")
+      model == "CM2_6" ~ "../Documents/MATLAB/CM2_6/CM26_hist_reshape.mat",
+      model == "CM2_1" ~ "../Documents/MATLAB/CM2_1/CM21m_season_ESRL.mat",
+      TRUE ~ paste0 ("../Documents/MATLAB/CMIP6/", model, "_hist_sst_reshape.mat")
                      )
-    model_name <- paste0 (model, "_mat")
     
     mat_list <- readMat (mat_file)
     
-    model_vals <- mat_list[[1]] # should be 1440 x 720 x 4
+    model_vals <- mat_list[[1]] # should be 1440 x 720 x 4 for CMIP6. CM2.6 is 140x240x12, north atl crop. 
     
-    model_ISL_crop <- array(model_vals[540:801, 601:661,])
+    dim(model_vals)
+    
+    model_ISL_crop <- array(model_vals) #[397:801,521:720,])
     
     # make dataframe
     TD_model <- data.frame (
@@ -96,3 +101,7 @@ cm_ensemble <- c ("CM2_6", "CNRM", "GFDL", "MOHC", "IPSL")
 plot_TD_fun (model_list = cm_ensemble,
              obs_var = "OISST", 
              figure_name = "TD_CM_Ensemble_OISST.png")
+
+plot_TD_fun (model_list = cm_ensemble,
+             obs_var = "OISST", 
+             figure_name = "TD_CM_Ensemble_OISST_Natl_check.png")
