4517e1f1fd3af4cc4d55b5195cb3e80c5ff948ef
Julia Mason <juliagmason@gmail.com>
2020-12-01 17:27:46 -0500

    Tweaked so I don't have to bring in mfri_pred

--- R/Map_MFRI_heatmaps.R
+++ R/Map_MFRI_heatmaps.R
@@ -22,30 +22,17 @@ mfri_abun <- read_csv ("Data/MFRI_comb_survey.csv",
 ) %>%
   filter (!(year == 2011 & season == "autumn"),
           !(year < 2000 & season == "autumn")) %>% # remove autumn 2011 [labor strike] and pre-2000 autumn [sites weren't standard yet]
+  select (-length_cm) %>% # this is the only remaining column that varies within sample_id
   group_by (sample_id, species) %>%
-  summarize (n_tot = sum(n_per_nautmile),
-             kg_tot = sum(kg_per_nautmile)) %>%
-  mutate (n_log = log(n_tot),
-          kg_log = log(kg_tot))
+  mutate (n_tot = sum(n_per_nautmile),
+          kg_tot = sum(kg_per_nautmile),
+          n_log = log (n_tot),
+          kg_log = log (kg_tot), 
+          .keep = "unused") %>% 
+  ungroup() %>%
+  distinct () 
 
-# predictor variables
-# I compiled these in Build_MFRI_predictor_df.R. 
-mfri_pred <-  read_csv ("Data/MFRI_predictor_df.csv",
-                        col_types = cols(
-                          sample_id = col_factor(),
-                          stat_sq = col_factor(),
-                          bormicon_region = col_factor(),
-                          sst_dev = col_number(),
-                          bt_dev = col_number(),
-                          sst_max = col_number(),
-                          sst_min = col_number(),
-                          bt_max = col_number(),
-                          bt_min = col_number()
-                        )
-) %>%
-  filter (!(year == 2011 & season == "autumn"),
-          !(year < 2000 & season == "autumn")
-  )  # remove autumn 2011, 131 samples. Also cutting out autumn pre-2000 (as of 7/23/2020. so did this with MASE fitting, but not with original run through of full gams)
+
 
 # species list
 # This relates species IDs to scientific names
@@ -64,14 +51,14 @@ plot_spp_map <- function (sci_name) {
   
   # filter points for species
   spp_pts <- mfri_abun %>%
-    filter (species == spp_id) %>%
-    left_join (mfri_pred, by = "sample_id")
+    filter (species == spp_id) 
   
   # also show centroid of distribution?
   centroid <- spp_pts %>%
     group_by (season) %>%
-    summarise (lon = weighted.mean (spp_pts$lon, spp_pts$kg_log),
-              lat = weighted.mean (spp_pts$lat, spp_pts$kg_log))
+    summarise (lon = weighted.mean (lon, log(n_tot + 1)),
+              lat = weighted.mean (lat, log(kg_tot + 1))
+    )
   
   # plot heatmap of log biomass
   ggplot(data = world) +
@@ -87,6 +74,9 @@ plot_spp_map <- function (sci_name) {
 
 plot_spp_map ("Artediellus_atlanticus")
 plot_spp_map ("Anarhichas_lupus")
+plot_spp_map ("Macrourus_berglax")
+plot_spp_map ("Sebastes_marinus")
+plot_spp_map ("Phycis_blennoides")
 
 # plot all together, color as warm/cool/cold, size as overall biomass?
 # and then could animate or show centroid shift
