1c04a17f2ea6fb9bc89f2da452504573838cf654
Julia Mason <juliagmason@gmail.com>
2021-01-31 17:49:42 -0500

    Brought over 2081 files from remote desktop. Should have them all now.

--- R/Download_gdrive_CM26.R
+++ R/Download_gdrive_CM26.R
@@ -26,7 +26,7 @@ for (file in cm_files$name) {
 test_br <- brick ("Models/Prediction_bricks/Bricks_2021/Leptoclinus_maculatus_Borm_14_alltemp_CM26_585_2021_2040.grd")
 
 
-
+cm_files <- drive_find (pattern = "_Borm_")
 
 for (file in cm_files$name) {
   file_path <- file.path("Models/Prediction_bricks", file)
@@ -35,7 +35,9 @@ for (file in cm_files$name) {
 
 files_21 <-  list.files (path = "Models/Prediction_bricks/", pattern = "2021") # want 882
 files_41 <- list.files (path = "Models/Prediction_bricks/", pattern = "2041")
+files_81 <- list.files (path = "Models/Prediction_bricks/", pattern = "2081")
 save (files_21, file = "files_21_local.RData")
 save (files_41, file = "files_41_local.RData")
+save (files_81, file = "files_81_local.RData")
 
 extra_21 <- drive_find (pattern = "Lepidorhombus_whiffiagonis_Borm_14_alltemp_gfdl_245_2021_2040.gri")
