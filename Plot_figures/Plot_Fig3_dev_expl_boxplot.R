cc28a826c317fa4c2f493681219977e0c868777b
Julia Mason <juliagmason@gmail.com>
2021-07-26 18:07:50 -0400

    New figure showing spread of variable contribution to deviance explained

---
+++ Plot_figures/Plot_Fig3_dev_expl_boxplot.R
@@ -0,0 +1,32 @@
+# Plot contribution to deviance explained, Fig 3
+# JGM
+
+library (tidyverse)
+
+
+# CSV of individual variable contribution to deviance explained. Made this table in Step1_Fit_GAMs. Focusing on the difference between dev explained in the full model and dev explained in a model with each variable dropped. That column is called "Diff_f"
+var_imp <- read_csv ("Models/var_imp_Rug_nb.csv")
+
+
+var_imp %>%
+  distinct() %>%
+  mutate (Var = case_when (
+    Var == "s(tow_depth_begin)" ~ "tow depth",
+    Var == "s(bottom_temp)" ~ "BT",
+    Var == "s(surface_temp)" ~ "SST",
+    TRUE ~ as.character (Var)
+  )) %>%
+  ggplot (aes (x = reorder(Var, Diff_f, median), y = Diff_f)) +
+  geom_boxplot () +
+  geom_jitter (size = 0.75) +
+  geom_hline (yintercept = 0, col = "red", lty = 2) +
+  coord_flip () +
+  theme_bw() +
+  labs (x = "", y = "% deviance explained, full model - covariate dropped model") +
+  theme (
+    axis.text = element_text (size = 8),
+    axis.title = element_text (size = 10)
+
+  )
+
+ggsave ("Figures/Var_imp_boxplot.eps", width = 85, height = 127.5, units = "mm", dpi = 300)
